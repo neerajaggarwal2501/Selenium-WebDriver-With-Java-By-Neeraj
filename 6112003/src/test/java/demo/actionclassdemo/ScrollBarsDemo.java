33e35353d13399ee25afa61a4e14cc20b0788b4b
Neeraj Aggarwal <neerajaggarwal2501@gmail.com>
2023-11-29 22:16:18 +0530

    .

---
+++ 6112003/src/test/java/demo/actionclassdemo/ScrollBarsDemo.java
@@ -0,0 +1,28 @@
+package demo.actionclassdemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ScrollBarsDemo {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/Users/NeerajAggarwal/git/SeleniumYT/6112003/src/test/resources/ScrollBars.html");
+		Actions actions = new Actions(driver);
+		
+		//Element 1 - Horizontal scroll
+		WebElement button1 = driver.findElement(By.xpath("//button[text()='Thirty']"));
+		
+		//Element 1 - Vertical scroll
+		WebElement button2 = driver.findElement(By.xpath("//button[text()='J']"));
+		
+		Thread.sleep(4000);
+		actions.scrollToElement(button1).perform();
+		
+		Thread.sleep(4000);
+		actions.scrollToElement(button2).perform();
+	
+	}
+}
