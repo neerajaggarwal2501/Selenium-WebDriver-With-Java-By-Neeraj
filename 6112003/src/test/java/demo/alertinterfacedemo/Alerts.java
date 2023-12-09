ad9ce5931b42d517f0c6299ba39cd65698eb5b41
Neeraj Aggarwal <neerajaggarwal2501@gmail.com>
2023-12-09 16:28:10 +0530

    How to use Alert interface methods to handle JavaScript based popups (Alert Box/Confirm Box/Prompt Box)

---
+++ 6112003/src/test/java/demo/alertinterfacedemo/Alerts.java
@@ -0,0 +1,42 @@
+package demo.alertinterfacedemo;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/Users/NeerajAggarwal/git/SeleniumYT/6112003/src/test/resources/JavaScriptPopupBoxes.html");
+		WebElement button1 = driver.findElement(By.xpath("//button[text()='Alert Box']"));
+		WebElement button2 = driver.findElement(By.xpath("//button[text()='Confirm Box']"));
+		WebElement button3 = driver.findElement(By.xpath("//button[text()='Prompt Box']"));
+		
+		button1.click();
+		Alert alert1 = driver.switchTo().alert();
+		System.out.println(alert1.getText());    		//1
+		Thread.sleep(3000);
+		alert1.accept();								//2
+	
+		button2.click();
+		Alert alert2 = driver.switchTo().alert();
+		Thread.sleep(3000);
+		System.out.println(alert2.getText());			//1
+		alert2.accept();								//2
+		
+		button2.click();
+		Alert alert3 = driver.switchTo().alert();
+		Thread.sleep(3000);
+		System.out.println(alert3.getText());			//1
+		alert3.dismiss();								//3
+		
+		button3.click();
+		Alert alert4 = driver.switchTo().alert();
+		System.out.println(alert4.getText());
+		Thread.sleep(2000);
+		alert4.sendKeys("Mickey Mouse");				//4
+		alert4.accept();								//2
+	}
+}
