829a40575517b11a84aab9964a7a00d57b7db972
Neeraj Aggarwal <neerajaggarwal2501@gmail.com>
2023-12-11 17:05:06 +0530

    .

--- 6112003/src/test/java/demo/javascriptexecutorinterfacedemo/JavaScriptExecutorInterfaceDemo.java
+++ 6112003/src/test/java/demo/javascriptexecutorinterfacedemo/JavaScriptExecutorInterfaceDemo.java
@@ -6,17 +6,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class JavaScriptExecutorInterfaceDemo_Click {
+public class JavaScriptExecutorInterfaceDemo {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		JavascriptExecutor jsExecutor = (JavascriptExecutor)driver;
 		driver.get("file:///C:/Users/NeerajAggarwal/git/SeleniumYT/6112003/src/test/resources/Click.html");
+		
+		//Getting the JavaScript Executor
+		JavascriptExecutor jsExecutor = (JavascriptExecutor)driver;
+		
 		WebElement button1 = driver.findElement(By.id("Question_India"));
 		Thread.sleep(3000);
 		
 		//Perform Button click using JavaScriptExecutor 
-		jsExecutor.executeScript("document.getElementById('Question_India').click()");    // Call JSExecutor by passing 1 argument
+		jsExecutor.executeScript("document.getElementById('Question_India').click()");   // Call JSExecutor by passing 1 argument
 		Thread.sleep(3000);
 
 		//Reload the page using JavaScriptExecutor
@@ -28,6 +31,6 @@ public class JavaScriptExecutorInterfaceDemo_Click {
 		button1 = driver.findElement(By.id("Question_India"));  
 		
 		//Perform Button click using JavaScriptExecutor
-		jsExecutor.executeScript("arguments[0].click();",button1);					// Call JSExecutor by passing multiple arguments
+		jsExecutor.executeScript("arguments[0].click();",button1);	// Call JSExecutor by passing multiple arguments
 	}
 }
