bfcfd9dc22a78350a1ca0d9005cd635aa4051e4c
Neeraj Aggarwal <neerajaggarwal2501@gmail.com>
2023-12-14 19:32:00 +0530

    Minor Updates

--- 6112003/src/test/java/demo/takescreenshotinterfacedemo/ScreenshotDemo.java
+++ 6112003/src/test/java/demo/takescreenshotinterfacedemo/ScreenshotDemo.java
@@ -3,6 +3,7 @@ package demo.takescreenshotinterfacedemo;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 
@@ -21,13 +22,25 @@ public class ScreenshotDemo {
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofMinutes(1));
 		
 		// Full Screenshot
-		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		Files.copy(srcFile.toPath(), Paths.get(System.getProperty("user.dir"),"screenshots",srcFile.getName()));
+		File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		Path sourcePath = screenshotFile.toPath();
+		Path destinationPath = Paths.get(System.getProperty("user.dir"),"screenshots",screenshotFile.getName());
+		Files.copy(sourcePath, destinationPath);
+
+		System.out.println("[Full Page] Source Path      :"  + sourcePath);
+		System.out.println("[Full Page] Destination Path :"  + destinationPath);
+		
 
 		// Screenshot of a WebElement - button1
-		WebElement button1 = driver.findElement(By.xpath("//button[@id='Source_Apple']"));
-		File srcFile1 = ((TakesScreenshot) button1).getScreenshotAs(OutputType.FILE);
-		Files.copy(srcFile1.toPath(), Paths.get(System.getProperty("user.dir"),"screenshots",srcFile1.getName()));
+		WebElement button_Apple = driver.findElement(By.id("Source_Apple"));
+		File screenshotFile1 = ((TakesScreenshot) button_Apple).getScreenshotAs(OutputType.FILE);
+		Path sourcePath1 = screenshotFile1.toPath();
+		Path destinationPath1 = Paths.get(System.getProperty("user.dir"),"screenshots",screenshotFile1.getName());
+		Files.copy(sourcePath1, destinationPath1);
+
+		System.out.println("[WebElement] Source Path      :"  + sourcePath1);
+		System.out.println("[WebElement] Destination Path :"  + destinationPath1);
+
 
 		Thread.sleep(2000);
 		driver.quit();
