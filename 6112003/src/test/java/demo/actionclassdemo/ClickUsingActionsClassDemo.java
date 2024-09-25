4124ab40ab3c387d29bfb7bb504a45cec501e4be
Neeraj Aggarwal <neeraj.aggarwal2501@gmail.com>
2024-09-25 17:54:37 +0530

    .

--- 6112003/src/test/java/demo/actionclassdemo/ClickUsingActionsClassDemo.java
+++ 6112003/src/test/java/demo/actionclassdemo/ClickUsingActionsClassDemo.java
@@ -10,12 +10,13 @@ public class ClickUsingActionsClassDemo {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		driver.get("file:///C:/Users/NeerajAggarwal/git/SeleniumYT/6112003/src/test/resources/Click.html");
-		
+		driver.get(System.getProperty("user.dir") + "/src/test/resources/Click.html");
+
 		Actions actions = new Actions(driver);
-		
+
 		WebElement button = driver.findElement(By.id("Question_India"));
 		Thread.sleep(3000);
 		actions.click(button).perform();
+		driver.quit();
 	}
 }
