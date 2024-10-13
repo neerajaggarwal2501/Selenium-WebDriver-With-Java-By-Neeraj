d22fe7bc71a44c770396673e9a6ec1b787d4687b
Neeraj Aggarwal <neeraj.aggarwal2501@gmail.com>
2024-10-13 15:38:22 +0530

    Added a reusable method to read data from excel sheet into a 2 dimensional String Array

--- 06112023/src/test/java/apachepoi/ReadExcel.java
+++ 06112023/src/test/java/apachepoi/ReadExcel.java
@@ -33,4 +33,37 @@ public class ReadExcel {
 			}
 		}
 	}
+
+	public static String[][] excelDataProvider(String fileName, String sheetName) throws IOException {
+
+		// Placeholder to output
+
+		String[][] dataArray;
+
+		// Open the Excel File in Java as a Stream
+		FileInputStream fis = new FileInputStream(Path.of(System.getProperty("user.dir"), "src", "test", "resources", "testdata", "excel", fileName).toString());
+
+		try (XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+
+			XSSFSheet sheet = workbook.getSheet(sheetName);
+			int rowCount = sheet.getLastRowNum() + 1;
+			int colCount = sheet.getRow(0).getLastCellNum();
+			dataArray = new String[rowCount][colCount];
+
+			// Create a DataFormatter to format and get each cell's value as String
+			DataFormatter dataFormatter = new DataFormatter();
+
+			for (Row row : sheet) {
+				for (Cell cell : row) {
+
+					String cellValue = dataFormatter.formatCellValue(cell); // Returns the formatted value of a cell as a String regardless of the cell type.
+																			// If the Excel format pattern cannot be parsed then the cell value will be formatted using a default format.
+					dataArray[row.getRowNum()][cell.getAddress().getColumn()] = cellValue;
+				}
+				System.out.println();
+			}
+		}
+
+		return dataArray;
+	}
 }
