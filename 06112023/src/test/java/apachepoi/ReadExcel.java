fe09103d894dd1c0e31ed087f9b23de21457b311
Neeraj Aggarwal <neeraj.aggarwal2501@gmail.com>
2024-10-13 07:38:38 +0530

    read Sheet by Name instead of Index

--- 06112023/src/test/java/apachepoi/ReadExcel.java
+++ 06112023/src/test/java/apachepoi/ReadExcel.java
@@ -13,23 +13,20 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ReadExcel {
 	public static void main(String[] args) throws IOException {
 		// Path of the excel file
-		FileInputStream fs = new FileInputStream(Path
-				.of(System.getProperty("user.dir"), "src", "test", "resources", "testdata", "excel", "TestData.xlsx")
-				.toString());
+		FileInputStream fs = new FileInputStream(Path.of(System.getProperty("user.dir"), "src", "test", "resources", "testdata", "excel", "TestData.xlsx").toString());
 
 		// Creating a workbook
 		try (XSSFWorkbook workbook = new XSSFWorkbook(fs)) {
-			XSSFSheet sheet = workbook.getSheetAt(1); // 0 based index
+			XSSFSheet sheet = workbook.getSheet("read");
 
 			// Create a DataFormatter to format and get each cell's value as String
 			DataFormatter dataFormatter = new DataFormatter();
 
 			for (Row row : sheet) {
 				for (Cell cell : row) {
-					// Returns the formatted value of a cell as a String regardless of the cell
-					// type. If the Excel format pattern cannot be parsed then the cell value will
-					// be formatted using a default format.
-					String cellValue = dataFormatter.formatCellValue(cell);
+
+					String cellValue = dataFormatter.formatCellValue(cell); // Returns the formatted value of a cell as a String regardless of the cell type.
+																			// If the Excel format pattern cannot be parsed then the cell value will be formatted using a default format.
 					System.out.print(cellValue + "\t");
 				}
 				System.out.println();
