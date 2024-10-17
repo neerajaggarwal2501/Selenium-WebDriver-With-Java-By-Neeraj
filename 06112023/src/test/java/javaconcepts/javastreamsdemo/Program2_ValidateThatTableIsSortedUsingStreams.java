1eaa133ec6717fa99db1615c284188791a7d33a5
Neeraj Aggarwal <neerajaggarwal2501@gmail.com>
2024-10-17 21:22:47 +0530

    Validate Table contents using streams

--- 06112023/src/test/java/javaconcepts/javastreamsdemo/Program2_ValidateThatTableIsSortedUsingStreams.java
+++ 06112023/src/test/java/javaconcepts/javastreamsdemo/Program2_ValidateThatTableIsSortedUsingStreams.java
@@ -25,6 +25,7 @@ public class Program2_ValidateThatTableIsSortedUsingStreams {
 		WebElement table_header_Country = driver.findElement(By.xpath("//th[text()='Country']"));
 		table_header_Country.click(); // This will sort by Country in Ascending order
 
+		// 1
 		System.out.println("Clicked on table header named Country");
 		List<WebElement> countriesList = driver.findElements(By.xpath("//table//tr/td[1]"));
 		List<String> actualListOfCountries = countriesList.stream().map(s -> s.getText()).toList();
@@ -32,6 +33,13 @@ public class Program2_ValidateThatTableIsSortedUsingStreams {
 		List<String> expectedListOfCountries = actualListOfCountries.stream().sorted().toList();
 		Assert.assertEquals(actualListOfCountries, expectedListOfCountries);
 
+		// 2 Print the Capital of Canada
+		countriesList.stream().filter(s -> s.getText().equals("India")).map(s -> getCapital(s)).forEach(s -> System.out.println(s));// New Delhi
+
 		driver.quit();
+	}
+
+	private static String getCapital(WebElement Country) {
+		return Country.findElement(By.xpath("following-sibling::td[1]")).getText();
 	}
 }
